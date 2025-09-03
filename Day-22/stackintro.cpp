013b8153c55ce34dd47f020fa5896b91b551a6c7
ShashwatS02 <shashawat15@gmail.com>
2025-09-04 01:42:54 +0530

    feat: added Day-22 folder with C++ practice files

---
+++ Day-22/stackintro.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//implement it with array
+class Stack {
+    int *arr;
+    int size;
+    int top;
+
+    public:
+
+    bool flag;
+    //Constructor
+    Stack(int s) {
+        size = s;
+        top = -1;
+        arr = new int[s];
+        flag = 1;
+    }
+
+    //Push
+    void push(int value) {
+        if (top == size-1) {
+            cout << "Stack Overflow\n";
+            return;
+        }
+        else {
+            top++;
+            arr[top] = value;
+            cout << "Pushed " << value << " into the stack\n";
+            flag = 0;
+        }
+    }
+    //Pop
+    void pop() {
+       if(top==-1) {
+        cout << "Stack Underflow\n";
+       }
+       else {
+        cout <<"Popped " << arr[top] << " from the stack\n";
+        top--;
+        if(top == -1) flag = 1;
+       } 
+    }
+    //Peek
+    int peek() {
+        if(top==-1) {
+        cout << "Stack is empty\n";
+        return -1;
+       }
+       else {
+        return arr[top];
+       }
+    }
+    //IsEmpty
+    bool IsEmpty() {
+        return top == -1;
+    }
+    //IsSize
+    int IsSize() {
+        return top+1;
+    }
+};
+
+int main() {
+
+    Stack S(5);
+    // S.push(-1);
+    int value = S.peek();
+    if (S.flag==0)
+    cout << value << endl;
+
+    return 0;
+}
